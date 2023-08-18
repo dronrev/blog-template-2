db05876ca1daff31aa4282240a03d54fe5e58db2
dronrev <vernordmusran27@gmail.com>
2023-08-18 11:53:38 +0800

    adding sidebar

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,11 +1,7 @@
 $(document).ready(function () {
-  // console.log('test')
   if (screen.width <= 768) {
     $(".nav-menu").hide();
-    $(".sidebar-menu").on('click',function(){
-        $(".sidebar-item").toggle();
-    });
   } else {
-    $(".sidebar-menu").hide();
+    $(".mobile-version-menu-button").hide();
   }
 });
